fd943f5d9e123ab6a3fe3899c9081c6da3cbdc95
Byron Stearns <bstearns@vizuri.com>
2023-08-02 16:06:43 -0400

    more urls

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,6 +1,6 @@
 export default {
     site: 'https://qa.weissta.org/',
-    urls: ['/about', '/events', '/newsletter', '/resources'],
+    urls: ['/about', '/events', '/newsletter', '/resources', '/equity', '/terms', '/accessibility', '/privacy'],
     debug: true,
     routerPrefix: 'weiss-unit-testing/unlighthouse',
     scanner: {
